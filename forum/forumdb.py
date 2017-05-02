4dc20b540b979ba0d8acec9ffd0a8c186d406236
Christiaan Quyn <christianq010@gmail.com>
2017-05-02 14:17:52 +0530

    feat: Add database to posts feature ( post into db and get from db)

--- forum/forumdb.py
+++ forum/forumdb.py
@@ -1,15 +1,29 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+
+# Get posts from database
 
 def get_posts():
+  # Connect to database.
+  db = psycopg2.connect("dbname=forum")
+  # Create cursor to sort
+  c = db.cursor()
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  c.execute("SELECT time, content FROM posts order by time DESC")
+  posts = ({'content': str(row[1]), 'time': str(row[0])}
+           for row in c.fetchall())
+  db.close()
+  return posts
+
+# Add Post to Database
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
-
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  c.execute("INSERT INTO posts (content) VALUES ('%s')" % content)
+  db.commit()
+  db.close()
