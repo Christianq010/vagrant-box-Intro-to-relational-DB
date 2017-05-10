32a99a4bcf17270e4ce4eff313ae8bcefc764e20
Christiaan Quyn <christianq010@gmail.com>
2017-05-10 19:17:36 +0530

    feat: For Creating A Database - Add Class And Table

--- restaurent-menu/database_setup.py
+++ restaurent-menu/database_setup.py
@@ -1,5 +1,6 @@
 # Creating a Database with SQLAlchemy ( our ORM for python - database mapping)
 # Step 1 - Configuration
+# Step 2 - Class
 
 import sys
 
@@ -12,9 +13,20 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+
+class Restaurent(Base):
+    __tablename__ = 'restaurent'
+
+class MenuItem(Base):
+    __table__ = 'menu_item'
+
+
+
 # Insert at end of file
 
 engine = create_engine('sqlite:///restaurentmenu.db')
 
 Base.metadata.create_all(engine)
+
 
