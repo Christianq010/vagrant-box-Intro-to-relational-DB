58c7c9388b95bbb92edcca445edc7ffb0f14454e
Christiaan Quyn <christianq010@gmail.com>
2017-05-11 00:02:17 +0530

    feat: Add Mapper for ORM
    Add mapper to each Class

--- restaurent-menu/database_setup.py
+++ restaurent-menu/database_setup.py
@@ -1,6 +1,7 @@
 # Creating a Database with SQLAlchemy ( our ORM for python - database mapping)
 # Step 1 - Configuration
 # Step 2 - Class
+# Step 3 - Mapper
 
 import sys
 
@@ -15,17 +16,27 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Restaurent(Base):
-    __tablename__ = 'restaurent'
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+
 
 class MenuItem(Base):
     __table__ = 'menu_item'
 
-
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    course = Column(String(250))
+    description = Column(String(250))
+    price = Column(String(8))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
 
 # Insert at end of file
 
-engine = create_engine('sqlite:///restaurentmenu.db')
+engine = create_engine('sqlite:///restaurantmenu.db')
 
 Base.metadata.create_all(engine)
 
